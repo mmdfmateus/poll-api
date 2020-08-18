15ef3f372f0e71f5ada350eb519481c6f995c9d3
Mateus Mourão <mmdfmateus@gmail.com>
2020-08-17 23:22:53 -0300

    refactor: extract sut build to a factory method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 import { SignUpController } from './signup';
 import { MissingParamError } from '../errors/missing-param-error';
 
+const makeSut = (): SignUpController => {
+  return new SignUpController();
+}
+
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', async () => {
-    const sut = new SignUpController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         email: 'any_mail@email.com',
@@ -18,7 +22,7 @@ describe('SignUp Controller', () => {
   });
 
   test('Should return 400 if no email is provided', async () => {
-    const sut = new SignUpController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         name: 'any name',
@@ -33,7 +37,7 @@ describe('SignUp Controller', () => {
   });
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = new SignUpController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         name: 'any name',
@@ -48,7 +52,7 @@ describe('SignUp Controller', () => {
   });
 
   test('Should return 400 if no passwordConfirmation is provided', async () => {
-    const sut = new SignUpController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         name: 'any name',
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -6,6 +6,7 @@ import { MissingParamError } from '../errors/missing-param-error';
 export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
