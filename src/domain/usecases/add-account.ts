00165085e3a9a10b6b6d9bad1c56cf51c5abe364
Mateus Mourão <mmdfmateus@gmail.com>
2020-08-20 00:20:38 -0300

    fix: remove async modifier from interface

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -7,5 +7,5 @@ export interface AddAccountModel {
 }
 
 export interface AddAccount {
-  async add(account: AddAccountModel): Promise<AccountModel>;
+  add(account: AddAccountModel): Promise<AccountModel>;
 }
