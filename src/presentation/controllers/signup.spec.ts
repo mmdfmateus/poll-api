f5689d6422e8548219c4d2ebe36a6577547003af
Mateus Mourão <mmdfmateus@gmail.com>
2020-08-19 21:23:42 -0300

    refactor: add index file to export all errors

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,6 @@
 import { SignUpController } from './signup';
-import { MissingParamError } from '../errors/missing-param-error';
-import { InvalidParamError } from '../errors/invalid-param-error';
+import { MissingParamError, InvalidParamError, InternalServerError } from '../errors';
 import { EmailValidator } from '../protocols/email-validator';
-import { InternalServerError } from '../errors/internal-server-error';
 
 interface SutTypes {
   sut: SignUpController,
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,8 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import { Controller } from '../protocols/controller';
 import { badRequest, internalServerError } from '../helpers/http-helpers'
-import { MissingParamError } from '../errors/missing-param-error';
+import { MissingParamError, InvalidParamError } from '../errors';
 import { EmailValidator } from '../protocols/email-validator';
-import { InvalidParamError } from '../errors/invalid-param-error';
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
