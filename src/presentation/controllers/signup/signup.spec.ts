531292d57d7189175170ad6efdc6db2561f0c3c2
Mateus Mourão <mmdfmateus@gmail.com>
2020-08-19 23:48:15 -0300

    test: ensure signup controller returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -14,7 +14,7 @@ const makeSut = (): SutTypes => {
   const sut = new SignUpController(emailValidatorStub, addAccountStub);
   return {
     sut,
-    emailValidatorStub, 
+    emailValidatorStub,
     addAccountStub
   };
 }
@@ -35,7 +35,7 @@ const makeAddAccountStub = (): AddAccount => {
       const fakeAccount = {
         _id: 'valid_id',
         name: 'valid_name',
-        email: 'email',
+        email: 'valid_mail@email.com',
         password: 'valid_password'
       };
       return fakeAccount;
@@ -211,5 +211,26 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new InternalServerError());
+  });
+
+  test('Should return 200 if valid data is provided', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_mail@email.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      _id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_mail@email.com',
+      password: 'valid_password'
+    });
   });
 });
