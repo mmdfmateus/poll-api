64521a2abda1c21a383bd2824b46177eb51f4042
Mateus Mourão <mmdfmateus@gmail.com>
2020-08-24 23:17:05 -0300

    tes: ensure DbAddAccount calls encrypter with correct param

---
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,36 @@
+import { DbAddAccount } from "./db-add-account";
+
+class EncrypterStub {
+  async encrypt(value: string): Promise<string> {
+    return new Promise(resolve => resolve('hashed_value'));
+  }
+}
+
+interface SutTypeValues {
+  sut: DbAddAccount,
+  encrypterStub: EncrypterStub
+}
+
+const makeSut = (): SutTypeValues => {
+  const encrypterStub = new EncrypterStub();
+  const sut = new DbAddAccount(encrypterStub);
+  return {
+    sut,
+    encrypterStub
+  };
+}
+
+describe('DbAddAccount UseCase', () => {
+  test('Should call Encrypter with correct param', async () => {
+    const { sut, encrypterStub } = makeSut();
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
+    const accountData = {
+      name: 'valid name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    };
+
+    sut.add(accountData);
+    expect(encryptSpy).toHaveBeenCalledWith('valid_password');
+  });
+});
