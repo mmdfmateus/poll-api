e23d0a2d70e3c9de23f1ac5d756b3e1a55193012
Mateus Mourão <mateusm@take.net>
2021-01-27 22:56:04 -0300

    refactor: extract sut build to a method

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -5,12 +5,18 @@ jest.mock('bcrypt', () => ({
     async hash(): Promise<string> {
         return new Promise(resolve => resolve('hashed_value'));
     }
-}))
+}));
+
+const salt = 12;
+
+const makeSut = (): BcryptAdapter => {
+    const sut = new BcryptAdapter(salt);
+    return sut;
+};
 
 describe('Bcrypt Adapter', () => {
     test('Should call bcrypt with correct values', async () => {
-        const salt = 12;
-        const sut = new BcryptAdapter(salt);
+        const sut = makeSut();
         const hashSpy = jest.spyOn(bcrypt, 'hash');
         
         await sut.encrypt('any_value');
@@ -19,8 +25,7 @@ describe('Bcrypt Adapter', () => {
     });
 
     test('Should return hash on success', async () => {
-        const salt = 12;
-        const sut = new BcryptAdapter(salt);
+        const sut = makeSut();
         
         const hash = await sut.encrypt('any_value');
 
